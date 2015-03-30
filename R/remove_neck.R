8de6133236479ca3f2ea9fa52e9ccd18dc9c4c5e
muschellij2 <muschellij2@gmail.com>
2015-03-30 11:12:20 -0400

    Moved remove_neck to extrantsr.

---
+++ R/remove_neck.R
@@ -0,0 +1,50 @@
+#' @title Remove Neck from CT Scan
+#'
+#' @description Removes the neck from axially acquired CT scan so
+#' Skull stripping can be done.
+#' @param file File for neck removal - either filename or class nifti
+#' @param template.file Template to warp to original image space
+#' @param template.mask Mask of template to use as rough brain mask
+#' @param ret_mask Return mask of slices to keep
+#' @param typeofTransform Transformation for template to image, passed to
+#' \code{\link{ants_regwrite}}.
+#' @param rep.value Value to replace neck slices with
+#' @param ... not used
+#' @export
+#' @return Object of class nifti or vector of indices
+remove_neck <- function(file, 
+	template.file = system.file("scct_unsmooth.nii.gz", 
+		package="cttools"),
+	template.mask = system.file("scct_unsmooth_Skull_Stripped_Mask.nii.gz", 
+		package="cttools"),
+	ret_mask = FALSE,
+	typeofTransform = "Rigid",
+  rep.value =0,
+  ...){
+
+	file = checkimg(file)
+	ofile = paste0(tempfile(), '.nii.gz')
+	ret = ants_regwrite(filename = template.file, template.file = file, 
+		typeofTransform=typeofTransform, other.files = template.mask, 
+		other.outfiles = ofile, retimg = TRUE, remove.warp = TRUE)
+
+	img = readNIfTI(file, reorient=FALSE)
+	mask = readNIfTI(ofile, reorient=FALSE)
+
+	ind = which(mask > 0.5, arr.ind=TRUE)
+	#5mm
+	# dimg = dim(img)
+	minz = min(ind[,"dim3"])
+	if (ret_mask) {
+    inds = seq(minz, dim(img)[3])
+    newimg = array(0, dim = dim(img))
+    newimg[,,inds] = 1	  
+    newimg = niftiarr(img, newimg)
+	} else {
+	  inds = seq(1, minz-1)
+	  newimg = img
+	  newimg@.Data[,,inds] = rep.value	  
+	  newimg = cal_img(newimg)
+	}
+	return(newimg)
+}
