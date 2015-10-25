9273c8f80efb484f3e4218c098c77b28d601ffe8
muschellij2 <muschellij2@gmail.com>
2015-10-25 14:00:48 -0400

    Adding otropos and antsCopy.

---
+++ R/antsCopy.R
@@ -0,0 +1,37 @@
+#' @title Copy Information of One Image to Another
+#' @description Wraps \code{antsSetOrigin/antsSetDirection/antsSetSpacing} 
+#' and \code{antsGetOrigin/antsGetDirection/antsGetSpacing}
+#' to copy the information over.  Finer control than 
+#' \code{antsCopyImageInfo}
+#'
+#' @param reference \code{antsImage} object to get origin values from
+#' @param target \code{antsImage} object to copy origin values to
+#'
+#' @return \code{target} object of class \code{antsImage}
+#' @export
+#' @name antsCopy
+#' @rdname antsCopy
+#' @examples
+#' img <- makeImage(c(10,10),rnorm(100))
+#' img2 <- makeImage(c(10,10), rnorm(100))
+#' img2 <- antsResetOrigin(img, img2)
+antsCopyOrigin = function(reference, target){
+  antsSetOrigin(target, as.numeric(antsGetOrigin(reference)))
+  return(target)
+}
+
+
+#' @rdname antsCopy
+#' @export
+antsCopyDirection = function(reference, target){
+  antsSetDirection(target, antsGetDirection(reference))
+  return(target)
+}
+
+#' @rdname antsCopy
+#' @export
+antsCopySpacing = function(reference, target){
+  antsSetSpacing(target, antsGetSpacing(reference))
+  return(target)
+}
+
---
+++ R/otropos.R
@@ -0,0 +1,43 @@
+#' @title Run Atropos for nifti objects
+#'
+#' @description Runs Atropos (a finite mixture model) but with more 
+#' robust inputs
+#' @param a Images to segment.  Can be a \code{nifti} object, 
+#' \code{antsImage} object, \code{character} vector, or list of these
+#' @param x Mask image.  If none is supplied and \code{make_mask = TRUE},
+#' then mask is created by first 
+#' @param ... Additional arguments to be passed to \code{\link{atropos}}
+#' @param make_mask Logical indicating if mask should be created if
+#' \code{x} is missing
+#' @param reset_orogin Logical indicating origin of mask should be 
+#' set to that of \code{a}
+#' @export
+#' @return Result of \code{\link{atropos}}, but with nifti objects.
+otropos <- function(a, 
+                     x, ..., 
+                     make_mask = TRUE,
+                     reset_origin = TRUE) {
+  if ( is.list(a) | is.character(a)  ) {
+    a = lapply(a, check_ants)
+    img = antsImageClone(a[[1]])
+  } else {
+    a = check_ants(a)
+    img = antsImageClone(a)
+  }
+  
+  if  (missing(x) ) {
+    if (make_mask) {
+      x = getMask(img)
+      args = list(x = x, ...)
+    } else {
+      args = list(...)
+    }
+  } else {
+    x = check_ants(x)
+    args = list(x = x, ...)
+  }
+  args = c(a = a, args)
+  res = do.call(atropos, args)
+  
+  return(res)
+}
