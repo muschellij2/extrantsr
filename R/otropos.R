bfe9affa16a2a6c900178535536cc5faf096d358
muschellij2 <muschellij2@gmail.com>
2015-10-25 14:44:03 -0400

    Fixing otropos.

--- R/otropos.R
+++ R/otropos.R
@@ -11,7 +11,8 @@
 #' 2D default atropos has. 
 #' @param ... Additional arguments to be passed to \code{\link{atropos}}
 #' @param make_mask Logical indicating if mask should be created if
-#' \code{x} is missing
+#' \code{x} is missing.  Mask is made using \code{\link{getMask}} with
+#' first image of \code{a}.
 #' @param reset_origin Should \code{\link{antsCopyOrigin}} be done
 #' on the mask?
 #' @export
@@ -30,7 +31,7 @@ otropos <- function(a,
     img = antsImageClone(a)
   }
   
-  if  (missing(x) ) {
+  if  (missing(x)) {
     if (make_mask) {
       x = getMask(img)
       args = list(x = x, ...)
@@ -49,6 +50,18 @@ otropos <- function(a,
   }
   args = c(a = a, args, m = m)
   res = do.call(atropos, args)
-  
+  if (!all(c("segmentation", "probabilityimages") %in% names(res))) {
+      warning(paste0("Results have non-standard output, cowardly ",
+                     "returning direct result of res"))
+  } else {
+      ants_out_seg = ants2oro(res$segmentation)
+      ants_out_seg = datatyper(ants_out_seg)
+      res$segmentation = ants_out_seg
+      for (i in seq_along( res$probabilityimages)) {
+        tmp = res$probabilityimages[[i]] 
+        tmp = ants2oro(tmp)
+        res$probabilityimages[[i]] = tmp
+      } 
+  }
   return(res)
 }
