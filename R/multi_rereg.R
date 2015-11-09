b40c2a25124d8c09ced274067b6e72d66c1a4866
muschellij2 <muschellij2@gmail.com>
2015-11-09 13:40:28 -0500

    Fixing multi_rereg.

--- R/multi_rereg.R
+++ R/multi_rereg.R
@@ -4,6 +4,8 @@
 #' takes a statistic image, and registers the list of images back to this 
 #' statistic image.
 #' @param infiles Input image files
+#' @param template.file Template image to register to for first pass.
+#' Second pass will be statistic image
 #' @param func Function to be passed to 
 #' @param ... Arguments to be passed to \code{\link{multi_reg}}. 
 #' @note The second stage will use the same arguments passed in \code{...}, but
@@ -12,10 +14,11 @@
 #' @return Output list of registered images to statistic image 
 #' and transformations
 multi_rereg <- function(infiles, 
-                      func = "mean",
-                      ...){
+                        template.file,
+                        func = "mean",
+                        ...){
   
-  res = multi_reg(infiles = infiles, ...)
+  res = multi_reg(infiles = infiles, template.file = template.file, ...)
   outfiles = lapply(res, function(x) x$outfile)
   run_stat_img = stat_img(outfiles, func = func)
   res = multi_reg(infiles = infiles, template.file = run_stat_img, ...)
