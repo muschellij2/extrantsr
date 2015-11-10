1ec9f2bec3cd4452de0e7584c0ee9ea243417eb3
muschellij2 <muschellij2@gmail.com>
2015-11-10 11:47:07 -0500

    Added mutual information.

---
+++ R/mutual_information.R
@@ -0,0 +1,17 @@
+#' @title Calculates Mutual Information on nifti objects
+#'
+#' @description Images are converted \code{nifti} objects
+#' (from the oro.nifti package), an operation is performed using 
+#' \code{\link{antsImageMutualInformation}}
+#' @param image1 Object of class \code{nifti} or character filename
+#' @param image2 Object of class \code{nifti} or character filename
+#' @param ... Additional arguments passed to 
+#' \code{\link{antsImageMutualInformation}}
+#' @export
+#' @import ANTsR
+#' @return Numeric
+mutual_information <- function(image1, image2, ...){
+  image1 = check_ants(image1)
+  image2 = check_ants(image2)
+  antsImageMutualInformation(image1, image2, ...)
+}
