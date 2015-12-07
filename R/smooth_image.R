8030572ca1304b815a306271795ab46b923ed079
muschellij2 <muschellij2@gmail.com>
2015-12-07 16:24:02 -0500

    Fixing smooth_image rd

--- R/smooth_image.R
+++ R/smooth_image.R
@@ -12,6 +12,7 @@
 #' @return Object of class \code{nifti}
 #' @examples
 #' system.time({
+#' library(oro.nifti)
 #' x = array(rnorm(1e6), dim = c(100, 100, 100))
 #' img = nifti(x, dim= c(100, 100, 100), 
 #' datatype = convert.datatype()$FLOAT32, cal.min = min(x), 
