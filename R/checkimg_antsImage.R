09ad44da4c8cf66f31e3497a1f93dc995bf11e43
muschellij2 <muschellij2@gmail.com>
2016-03-09 23:42:06 -0500

    added checkimg shit.

--- R/checkimg_antsImage.R
+++ R/checkimg_antsImage.R
@@ -1,3 +1,12 @@
+#' @name checkimg-methods
+#' @title Force object to filename 
+#' @description Ensures the output to be a character filename (or vector) from an input
+#' image or \code{nifti}.  
+#' @docType methods 
+#' @rdname checkimg-methods
+#' @aliases checkimg,antsImage-method
+#' @param file character or \code{nifti} object
+#' @param ... options passed to \code{\link{tempimg}} 
 #' @export
 setMethod(f = "checkimg", signature(file = "antsImage"), definition = function(file, ...) { 
   file = tempants(file, ...)
