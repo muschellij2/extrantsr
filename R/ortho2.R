0ccccad7cfd8d3d324ac422bc2ed7ac8617cdf56
muschellij2 <muschellij2@gmail.com>
2016-07-13 16:50:27 -0400

    Added ortho2 for extrantsr.

---
+++ R/ortho2.R
@@ -0,0 +1,24 @@
+#' @title Orthographic Display, added options
+#' @description Wrapper for of \code{fslr}'s \code{\link{ortho2}} function 
+#' to work with antsImages
+#' @return NULL
+#' @param x is an object of class \code{antsImage} or similar.
+#' @param y is an object of class \code{antsImage} or similar for the overlay.
+#' @param pdim Pixel dimensions if passing in arrays.  
+#' @param ... other arguments to the image function may be provided here.
+#' @note This will NOT do \code{\link{ants2oro}} to plot the image.  
+#' If that is desired, users will have to do that before calling 
+#' \code{ortho2}.
+#' @export
+ortho2 = function(x, y = NULL, pdim = NULL, ...) {
+  if (is.antsImage(x)) {
+    pdim = c(1, antsGetSpacing(x))
+    x = as.array(x)
+  }
+  if (!is.null(y)) {
+    if (is.antsImage(y)) {
+      y = as.array(y)
+    }
+  }  
+  fslr::ortho2(x = x, y = y, pdim = pdim, ...)
+}
