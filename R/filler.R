f05de07074bcc99977f1ae6d73f6d97dff080a6c
muschellij2 <muschellij2@gmail.com>
2017-01-23 11:46:09 -0500

    Added options to filler.

--- R/filler.R
+++ R/filler.R
@@ -4,12 +4,18 @@
 #' @param img Object of class \code{\link{nifti}} or \code{antsImage}
 #' @param fill_size Size of fill (in voxels)
 #' @param verbose Output diagnostic values
+#' @param dilate should the image be dilated.  This and \code{erode} 
+#' allows for the function to perform erosion/dilation only with zero-padding.
+#' @param erode should the image be eroded  This and \code{dilate} 
+#' allows for the function to perform erosion/dilation only with zero-padding.
 #' @return Filled object with same class as \code{img}
 #' @export
 #' @import ANTsR
 filler <- function(img, 
-                  fill_size = 7,
-                  verbose = TRUE){
+                   fill_size = 7,
+                   verbose = TRUE,
+                   dilate = TRUE,
+                   erode = TRUE){
   # kdim Dimensions to be padded to the image (in voxels)
   kdim = rep(fill_size, 3) * 2 + 1
   
@@ -37,17 +43,29 @@ filler <- function(img,
     return(arr)
   }
   
+  ensure_array = function(x) {
+    x = x %>% 
+      as.array %>% 
+      as(Class = "array")
+    return(x)
+  }
+  
   filled = img %>% 
-    as.array %>% 
-    as(Class = "array") %>% 
+    ensure_array %>% 
     zero_pad(kdim = kdim) %>% 
-    cnif(img = img,  verbose = verbose) %>% 
-    oMath("MD", fill_size, 
-          retfile = TRUE) %>% 
-    oMath("ME", fill_size, 
-          retfile = TRUE) %>% 
-    as.array %>% 
-    as(Class = "array") %>%             
+    cnif(img = img,  verbose = verbose) 
+  if (dilate) {
+    filled = filled %>% 
+      oMath("MD", fill_size, 
+            retfile = TRUE) 
+  }
+  if (erode) {
+    filled = filled %>% 
+      oMath("ME", fill_size, 
+            retfile = TRUE) 
+  } 
+  filled = filled %>% 
+    ensure_array %>%          
     zero_pad(
       kdim = kdim, 
       invert = TRUE) %>% 
