687af53debd3df79876e3580c9e6c966107e637f
muschellij2 <muschellij2@gmail.com>
2017-05-18 10:48:44 -0400

    Fixing some docs.

--- R/applyEmptyImageDimensions.R
+++ R/applyEmptyImageDimensions.R
@@ -8,9 +8,6 @@
 #' dropping empty dimensions.  
 #' 
 #' @param img image, nifti object, or array
-#' @param inds indices of subset from \code{\link{getEmptyImageDimensions}} or
-#' \code{\link{dropEmptyImageDimensions}}.
-#' @param reorient Should image be reoriented if a filename?
 #' @param ... not used
 #' @return Object of class \code{nifti} or \code{array} if \code{nifti}
 #' is not supplied
