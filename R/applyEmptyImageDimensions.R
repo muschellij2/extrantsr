e44c2bb86f9e69f23e5f7f88e9857409a9a01459
muschellij2 <muschellij2@gmail.com>
2017-05-18 17:27:06 -0400

    Fixing docs - should run now.

--- R/applyEmptyImageDimensions.R
+++ R/applyEmptyImageDimensions.R
@@ -10,6 +10,7 @@
 #' @param img image, nifti object, or array
 #' @param inds indices of subset from \code{\link{getEmptyImageDimensions}} or
 #' \code{\link{dropEmptyImageDimensions}}. 
+#' @param reorient Should image be reoriented if a filename 
 #' @param ... not used
 #' @return Object of class \code{nifti} or \code{array} if \code{nifti}
 #' is not supplied
@@ -20,6 +21,7 @@
 setMethod("applyEmptyImageDimensions", "antsImage", 
           function(img, 
                    inds,
+                   reorient = FALSE,
                    ...) {
             arr = as.array(img)
             res = neurobase::applyEmptyImageDimensions(
