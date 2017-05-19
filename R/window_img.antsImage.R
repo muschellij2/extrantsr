68698e1cb6b1d6295fb8df7dd0735d663e87e61e
muschellij2 <muschellij2@gmail.com>
2017-05-19 10:07:36 -0400

    Adding in window_img for antsImages.

---
+++ R/window_img.antsImage.R
@@ -0,0 +1,9 @@
+#' @export
+#' @method window_img antsImage
+#' @importFrom neurobase window_img 
+window_img.antsImage = function(x, ...) {
+  arr = as.array(x)
+  arr = neurobase::window_img(arr, ...)
+  arr = ANTsR::as.antsImage(arr, reference = x)
+  return(arr)
+}
