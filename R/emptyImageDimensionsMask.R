fb72c179dc061c35258871333d4e2311b99035e7
muschellij2 <muschellij2@gmail.com>
2017-06-04 18:20:53 -0400

    Added emptyImageDimensionsMask

---
+++ R/emptyImageDimensionsMask.R
@@ -0,0 +1,35 @@
+#' @rdname emptyImageDimensionsMask-methods
+#' @aliases emptyImageDimensionsMask,nifti-method
+#' 
+#' @docType methods 
+#' 
+#' @title Make Mask from Empty Image Dimensions
+#' @description Make a mask of an image that has all irrelevant
+#' values 
+#' @name emptyImageDimensionsMask
+#' @param img nifti object
+#' @param ... Arguments to be passed to \code{\link{getEmptyImageDimensions}}.
+#' @param reorient Should image be reoriented if a filename?
+#' 
+#' @return Object of class \code{nifti}, with binary values
+#' 
+#' @note \code{empty_dim_mask} is a shorthand for \code{emptyImageDimensionsMask}
+#' with all the same arguments.qQac vgfrew
+#' 
+#' @seealso \code{\link{getEmptyImageDimensions}}  
+#' @export
+#' @importFrom neurobase emptyImageDimensionsMask
+setMethod("emptyImageDimensionsMask", "antsImage", 
+          function(img, 
+                   ...,
+                   reorient = FALSE) { 
+            arr = as.array(img)
+            res = neurobase::emptyImageDimensionsMask(
+              img = arr, 
+              ..., 
+              reorient = reorient
+              )
+            res = ANTsRCore::as.antsImage(res)
+            res = ANTsRCore::antsCopyImageInfo(reference = img, target = res)
+            return(res)
+          })
