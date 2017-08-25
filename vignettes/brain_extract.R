bce52a4a0c9794319e17d6114ad056350b360a02
muschellij2 <muschellij2@gmail.com>
2017-08-25 17:51:02 -0400

    New extrantsr becuse neuroc failed.

--- vignettes/brain_extract.R
+++ vignettes/brain_extract.R
@@ -19,35 +19,3 @@ t1_fname = get_t1_filenames(
   ids = 113, 
   visits = 1)
 
-## ----t1_plot-------------------------------------------------------------
-t1 = readnii(t1_fname)
-ortho2(t1)
-
-## ----t1_naive_ss, cache = FALSE------------------------------------------
-if (have.fsl()) {
-  ss_naive = fslbet(infile = t1_fname)
-}
-
-## ----t1_naive_plot, cache = FALSE----------------------------------------
-if (have.fsl()) {
-  ortho2(ss_naive)
-}
-
-
-## ----t1_ss, cache = FALSE------------------------------------------------
-n4img = bias_correct(t1_fname, 
-                     correction = "N4", retimg = TRUE,
-                     verbose = FALSE)
-if (have.fsl()) {
-  template.file = file.path(fsldir(), "data/standard",
-                            "MNI152_T1_1mm_brain.nii.gz") 
-  template.mask = file.path(fsldir(),
-                            "data/standard", "MNI152_T1_1mm_brain_mask.nii.gz")  
-  removed_neck = extrantsr::double_remove_neck(
-    n4img,
-    template.file = template.file,
-    template.mask = template.mask)
-  ortho2(removed_neck)
-  double_ortho(t1, removed_neck)
-}
-
