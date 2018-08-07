3e0f8e0b11481587c886c98dddba994453c8526d
muschellij2 <muschellij2@gmail.com>
2018-08-07 15:05:25 -0400

    Added an argument to transformlist_from_outprefix.

--- R/transformlist_from_outprefix.R
+++ R/transformlist_from_outprefix.R
@@ -2,6 +2,7 @@
 #'
 #' @param outprefix If you used this in \code{\link{antsRegistration}}, then
 #' what would be the filenames of the transforms
+#' @param only_exists if \code{TRUE}, only those that exists will be returned.
 #'
 #' @return A list of character vectors
 #' @export
@@ -9,12 +10,21 @@
 #' @examples
 #' outprefix = tempfile()
 #' transformlist_from_outprefix(outprefix)
-transformlist_from_outprefix = function(outprefix) {
+transformlist_from_outprefix = function(outprefix,
+                                        only_exists = FALSE) {
   out_trans = c(
     Affine = "0GenericAffine.mat", 
     fwd = "1Warp.nii.gz", 
     inv = "1InverseWarp.nii.gz")
-  l = list(fwdtransforms = paste0(outprefix, out_trans[c("fwd", "Affine")]),
-           invtransforms = paste0(outprefix, out_trans[c("Affine", "inv")]))
+  fwdtransforms = paste0(outprefix, out_trans[c("fwd", "Affine")])
+  invtransforms = paste0(outprefix, out_trans[c("Affine", "inv")])
+  
+  if (only_exists) {
+    fwdtransforms = fwdtransforms[file.exists(fwdtransforms)]
+    invtransforms = invtransforms[file.exists(invtransforms)]
+  }
+  
+  l = list(fwdtransforms = fwdtransforms,
+           invtransforms = invtransforms)
   return(l)
 }
