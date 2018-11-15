812498a959fff9dab1d5f6ec306e952945d26370
muschellij2 <muschellij2@gmail.com>
2018-11-15 15:56:04 -0500

    added to the check_ants

--- R/check_ants.R
+++ R/check_ants.R
@@ -1,3 +1,8 @@
+#' @title Check if antsImage or read in
+#' @description Simple check to see if input is character, list, \code{nifti},
+#' or class \code{antsImage}
+#' @return antsImage object
+#' 
 #' @rdname check_ants-methods
 #' @aliases check_ants,nifti-method
 #' @importFrom ANTsRCore check_ants
--- R/transformlist_from_outprefix.R
+++ R/transformlist_from_outprefix.R
@@ -2,6 +2,7 @@
 #'
 #' @param outprefix If you used this in \code{\link{antsRegistration}}, then
 #' what would be the filenames of the transforms
+#' @param typeofTransform transformation to be used in registration
 #' @param only_exists if \code{TRUE}, only those that exists will be returned.
 #'
 #' @return A list of character vectors
@@ -10,21 +11,51 @@
 #' @examples
 #' outprefix = tempfile()
 #' transformlist_from_outprefix(outprefix)
-transformlist_from_outprefix = function(outprefix,
-                                        only_exists = FALSE) {
-  out_trans = c(
+#' transformlist_from_outprefix(outprefix, typeofTransform = "Rigid")
+#' res = transformlist_from_outprefix(outprefix, typeofTransform = "Rigid",
+#' only_exists = TRUE)
+#' stopifnot(is.null(res))
+transformlist_from_outprefix = function(
+  outprefix,
+  typeofTransform = "SyN",
+  only_exists = FALSE) {
+  if (is.null(typeofTransform)) {
+    typeofTransform = "SyN"
+  }
+  have_warp = (typeofTransform %in%
+                 c("Translation", "Rigid", "Similarity", "TRSAA")) |
+    grepl("Rigid", typeofTransform) |
+    grepl("Affine", typeofTransform)
+  have_warp = !have_warp
+  
+  out_trans = list(
     Affine = "0GenericAffine.mat", 
-    fwd = "1Warp.nii.gz", 
-    inv = "1InverseWarp.nii.gz")
-  fwdtransforms = paste0(outprefix, out_trans[c("fwd", "Affine")])
-  invtransforms = paste0(outprefix, out_trans[c("Affine", "inv")])
+    fwd = "1Warp.nii.gz",
+    inv = "1InverseWarp.nii.gz"
+  )
+  if (!have_warp) {
+    out_trans$fwd = out_trans$inv = NULL
+  }
+  out_trans = out_trans[ out_trans != ""]
+  fwdtransforms = paste0(outprefix, c(out_trans$fwd, out_trans$Affine))
+  invtransforms = paste0(outprefix, c(out_trans$Affine, out_trans$inv))
   
   if (only_exists) {
     fwdtransforms = fwdtransforms[file.exists(fwdtransforms)]
+    if (length(fwdtransforms) == 0) {
+      fwdtransforms = NULL
+    }
     invtransforms = invtransforms[file.exists(invtransforms)]
+    if (length(invtransforms) == 0) {
+      invtransforms = NULL
+    }    
   }
   
-  l = list(fwdtransforms = fwdtransforms,
-           invtransforms = invtransforms)
+  l = list()
+  l$fwdtransforms = fwdtransforms
+  l$invtransforms = invtransforms
+  if (length(l) == 0) {
+    return(NULL)
+  }
   return(l)
 }
